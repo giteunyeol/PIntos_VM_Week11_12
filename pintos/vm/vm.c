c9af29a9ccbafe1af94d44c209927b5140645e7b
sijun-yang <yangsijun5528@gmail.com>
2026-05-13 00:18:45 +0900

    좀 수정했는데 여전히 실패

--- pintos/vm/vm.c
+++ pintos/vm/vm.c
@@ -316,41 +316,19 @@ supplemental_page_table_copy (struct supplemental_page_table *dst,
 		struct page *src_page = hash_entry (hash_cur (&i), struct page, elem);
 		void *uva = src_page->va; // 둘 다 동일한 user virtual addr을 공유
 
-		//TODO: with_init 이 필요할지도? 왜냐면 uninit인 경우 lazy_load 가 필요할 수 있음
-		// uninit인 경우를 고려해서 분기 처리가 필요할수도?
-		// 근데 그건 해당 영역이 세그먼트인 경우만 포함되는데? 그러면 page 타입에 따라 page 내용 자체를 복사하고,
-		// frame만 새로 생성해야 할 수도?
-		vm_alloc_page (page_get_type (src_page), uva, src_page->writeable);
-		struct page *dst_page = spt_find_page (dst, uva);
-		ASSERT (dst_page != NULL); // must exists
+		// 생성 타입이 아니라 현재의 타입(상태,구조체?)를 가져옴
+		bool is_uninit = VM_TYPE (src_page->operations->type) == VM_UNINIT;
 
-		enum vm_type src_cur_type = VM_TYPE (src_page->operations->type);
-		void *aux;
-		switch (src_cur_type) {
-			case VM_ANON:
-				aux = src_page->uninit.aux;
-				break;
-			case VM_FILE:
-				PANIC ("IDK WTD");
-				break;
-		#ifdef EFILESYS  /* For project 4 */
-			case VM_PAGE_CACHE:
-				PANIC ("IDK WTD");
-				break;
-		#endif
-			default:
-				PANIC ("Unsupported VM Type(%d)", src_cur_type);
-				break;
-		}
-
-		bool has_src_frame = src_cur_type != VM_ANON;
-		if (has_src_frame) {
-			vm_alloc_page (page_get_type (src_page), uva, src_page->writeable);
-			vm_claim_page (uva);
-			memcpy (dst_page->frame->kva, src_page->frame->kva, PGSIZE);
+		if (is_uninit) {
+			vm_alloc_page_with_initializer(page_get_type(src_page), uva,
+			        src_page->writeable, src_page->uninit.init, src_page->uninit.aux);
 		} else {
-			vm_alloc_page_with_initializer (page_get_type (src_page), uva,
-					src_page->writeable, src_page->operations->swap_in, aux);
+			vm_alloc_page(page_get_type (src_page), uva, src_page->writeable);
+			vm_claim_page(uva);
+			struct page* dst_page = spt_find_page(dst, uva);
+			ASSERT(dst_page != NULL);
+			// must exists
+			memcpy(dst_page->frame->kva, src_page->frame->kva, PGSIZE);
 		}
 	}
 	DEG_RETURN ("value=true");
