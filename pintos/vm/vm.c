cd109f2bc381ceef7a6ec7b8ffe8668a43a5159b
sijun-yang <yangsijun5528@gmail.com>
2026-05-12 23:43:07 +0900

    중간 커밋 - 뭔가 갈아엎기 전이라

--- pintos/vm/vm.c
+++ pintos/vm/vm.c
@@ -315,6 +315,7 @@ supplemental_page_table_copy (struct supplemental_page_table *dst,
 	while (hash_next (&i)) {
 		struct page *src_page = hash_entry (hash_cur (&i), struct page, elem);
 		void *uva = src_page->va; // 둘 다 동일한 user virtual addr을 공유
+
 		//TODO: with_init 이 필요할지도? 왜냐면 uninit인 경우 lazy_load 가 필요할 수 있음
 		// uninit인 경우를 고려해서 분기 처리가 필요할수도?
 		// 근데 그건 해당 영역이 세그먼트인 경우만 포함되는데? 그러면 page 타입에 따라 page 내용 자체를 복사하고,
@@ -323,7 +324,27 @@ supplemental_page_table_copy (struct supplemental_page_table *dst,
 		struct page *dst_page = spt_find_page (dst, uva);
 		ASSERT (dst_page != NULL); // must exists
 
-		bool has_src_frame = src_page->frame != NULL;
+		enum vm_type src_cur_type = VM_TYPE (src_page->operations->type);
+		enum vm_type src_type = page_get_type (src_page);
+		switch (src_cur_type) {
+			case VM_ANON:
+				uninit_new (dst_page, uva, src_page->uninit.init, src_type,
+						src_page->uninit.aux, src_page->uninit.page_initializer);
+				break;
+			case VM_FILE:
+				vm_file_init()
+				break;
+			#ifdef EFILESYS  /* For project 4 */
+			case VM_PAGE_CACHE:
+				PANIC ("NOT NOW");
+				break;
+			#endif
+			default:
+				PANIC ("Unsupported VM Type(%d)", src_cur_type);
+				break;
+		}
+
+		bool has_src_frame = src_cur_type != VM_ANON;
 		if (has_src_frame) {
 			vm_claim_page (uva);
 			memcpy (dst_page->frame->kva, src_page->frame->kva, PGSIZE);
