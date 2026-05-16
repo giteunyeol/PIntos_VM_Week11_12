275f4c30020a80ed210b813341b692f9cd282409
giteunyeol <163628847+giteunyeol@users.noreply.github.com>
2026-05-16 20:53:09 +0900

    build complete

--- pintos/userprog/process.c
+++ pintos/userprog/process.c
@@ -1010,7 +1010,7 @@ load_segment (struct file *file, off_t ofs, uint8_t *upage,
 	ASSERT ((read_bytes + zero_bytes) % PGSIZE == 0);
 	ASSERT (pg_ofs (upage) == 0);
 	ASSERT (ofs % PGSIZE == 0);
-
+	
 	while (read_bytes > 0 || zero_bytes > 0) {
 		/* 이 페이지를 어떻게 채울지 계산한다.
 		 * FILE에서 PAGE_READ_BYTES 바이트를 읽고
